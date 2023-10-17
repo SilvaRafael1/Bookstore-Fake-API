9283aef24bf3af96a82710f41524a6619a776e32
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2023-10-17 15:52:16 -0300

    style: Adicionado estilos para as páginas

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import GetBooks from "./GetBooks"
 import CreateBook from "./CreateBook"
 import BookDetail from "./BookDetail"
 import UpdateBook from "./UpdateBook"
+import { AppBar, Toolbar, Typography } from "@mui/material"
+import NavBar from "./NavBar"
+import "./css/App.css"
 
 function App() {
     return (
         <BrowserRouter>
             <div>
-                <Routes>
-                    <Route exact path="/books" Component={GetBooks} />
-                    <Route path="/books/:id" Component={BookDetail} />
-                    <Route path="/updateBook/:id" Component={UpdateBook} />
-                    <Route path="/createBook" Component={CreateBook} />
-                </Routes>
+                <AppBar color="primary" position="static">
+                    <Toolbar>
+                        <Typography variant="h4" color="inherit">Livraria IFRS-BG</Typography>
+                        <NavBar />
+                    </Toolbar>
+                </AppBar>
+
+                <div className="routes">
+                    <Routes>
+                        <Route path="/books" Component={GetBooks} />
+                        <Route path="/books/:id" Component={BookDetail} />
+                        <Route path="/updateBook/:id" Component={UpdateBook} />
+                        <Route path="/createBook" Component={CreateBook} />
+                    </Routes>
+                </div>
             </div>
         </BrowserRouter>
     )
