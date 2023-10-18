0c266818b6445c4c5f32b4611f3a7f47e9442022
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2023-10-18 15:31:24 -0300

    feat: BookDetail style

--- src/BookDelete.jsx
+++ src/BookDelete.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
 import client from "./api/api";
 
 function BookDeleteButton({ id }) {
-    const [mensagem, setMensagem] = useState("")
     
     const handleDelete = async () => {
         try {
             await client.delete(`/${id}`)
-            setMensagem(`Livro com o ID ${id} deletado com sucesso!`)
+            console.log(`Livro com o ID ${id} deletado com sucesso!`)
+            alert(`Livro com o ID ${id} deletado com sucesso!`)
         } catch (err) {
             console.error('Erro ao deletar livro: ', err)
-            setMensagem(`Erro ao deletar livro: ${err}`)
         }
     }
 
     return (
         <div>
             <button onClick={handleDelete}>Deletar</button>
-            <div className="mensagem">{mensagem}</div>
         </div>
     )
 }
--- src/BookDetail.jsx
+++ src/BookDetail.jsx
@@ -1,6 +1,8 @@
 import { NavLink, useParams } from "react-router-dom";
 import BookDetailHooks from "./hooks/BookDetailHooks";
 import BookDelete from "./BookDelete";
+import "./css/BookDetail.css"
+import "./css/NavLinkStyle.css"
 
 function BookDetail() {
     const { id } = useParams()
@@ -10,16 +12,18 @@ function BookDetail() {
     if (error) return <div>{error}</div>;
 
     return (
-        <div>
-            <div className="book">
-                <p className="titulo">Título: {book.title} <br /></p>
-                <p className="descricao">Descrição: {book.description} <br /></p>
-                <p className="resumo">Excerto: {book.excerpt} <br /> </p>
-                <p className="num">Número de Páginas: {book.pageCount}</p>
-            </div>
-            <div className="btns">
-                <button><NavLink to={"/updateBook/" + book.id}>Atualizar</NavLink></button>
-                <BookDelete id={book.id} />
+        <div className="text-area">
+            <div className="text">
+                <div className="book">
+                    <p className="titulo">Título: {book.title} <br /></p>
+                    <p className="descricao">Descrição: {book.description} <br /></p>
+                    <p className="resumo">Excerto: {book.excerpt} <br /> </p>
+                    <p className="num">Número de Páginas: {book.pageCount}</p>
+                </div>
+                <div className="btns">
+                    <NavLink to={"/updateBook/" + book.id} className="nav-link"><button>Atualizar</button></NavLink>
+                    <BookDelete id={book.id} />
+                </div>
             </div>
         </div>
     )
