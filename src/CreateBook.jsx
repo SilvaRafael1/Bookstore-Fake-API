b1b7031a6187b72ec3f88d986457261a6547698a
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2023-10-18 15:08:07 -0300

    feat: Estilos Forms

--- src/CreateBook.jsx
+++ src/CreateBook.jsx
@@ -1,18 +1,16 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import GetBooksHooks from "./hooks/GetBooksHooks";
-import { useState } from "react";
 import client from "./api/api";
+import "./css/Form.css"
+import { Typography } from "@mui/material";
 
 function CreateBook() {
-    const [response, setResponse] = useState();
-
-    // const handleSubmit = (data) => console.log(JSON.stringify(data));
-    const handleSubmit = (data) => client.post("", data).then((responseData) => {
-        setResponse(responseData.data)
-        console.log(responseData.data)
+    const handleSubmit = (data) => client.post("", data).then(() => {
+        console.log("Livro criado com sucesso: ", data)
+        alert("Livro criado com sucesso! Para mais detalhes consulte o log")
     }).catch((err) => console.log(err));
-    
+
     const { books } = GetBooksHooks();
 
     const validationSchema = Yup.object().shape({
@@ -37,37 +35,42 @@ function CreateBook() {
     initialValues.id = lastId
 
     return (
-        <div>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                {({ errors, touched, resetForm }) => (
-                    <Form>
-                        <div className="form-group">
-                            <label htmlFor="title">Título</label>
-                            <Field name="title" id="title" type="text" className={'form-control' + (errors.title && touched.title ? 'is-invalid' : '')} />
-                            <ErrorMessage name="title" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="description">Descrição</label>
-                            <Field name="description" id="description" type="text" className={'form-control' + (errors.description && touched.description ? 'is-invalid' : '')} />
-                            <ErrorMessage name="description" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="excerpt">Excerto</label>
-                            <Field name="excerpt" id="excerpt" type="text" className={'form-control' + (errors.excerpt && touched.excerpt ? 'is-invalid' : '')} />
-                            <ErrorMessage name="excerpt" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="pageCount">Número de Páginas</label>
-                            <Field name="pageCount" id="pageCount" type="number" className={'form-control' + (errors.pageCount && touched.pageCount ? 'is-invalid' : '')} />
-                            <ErrorMessage name="pageCount" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit">Enviar</button>
-                            <button type="button" onClick={resetForm}>Limpar</button>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
+        <div className="form-area">
+            <div className="form">
+                <Typography variant="h4">
+                    Formulário de Criação de Livro
+                </Typography>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                    {({ errors, touched, resetForm }) => (
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="title">Título: </label>
+                                <Field name="title" id="title" type="text" className={'form-control' + (errors.title && touched.title ? 'is-invalid' : '')} />
+                                <ErrorMessage name="title" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="description">Descrição: </label>
+                                <Field name="description" id="description" type="text" className={'form-control' + (errors.description && touched.description ? 'is-invalid' : '')} />
+                                <ErrorMessage name="description" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="excerpt">Excerto: </label>
+                                <Field name="excerpt" id="excerpt" type="text" className={'form-control' + (errors.excerpt && touched.excerpt ? 'is-invalid' : '')} />
+                                <ErrorMessage name="excerpt" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="pageCount">Número de Páginas: </label>
+                                <Field name="pageCount" id="pageCount" type="number" className={'form-control' + (errors.pageCount && touched.pageCount ? 'is-invalid' : '')} />
+                                <ErrorMessage name="pageCount" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group" id="btns">
+                                <button type="submit">Enviar</button>
+                                <button type="button" onClick={resetForm}>Limpar</button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </div>
     );
 }
--- src/UpdateBook.jsx
+++ src/UpdateBook.jsx
@@ -4,6 +4,8 @@ import client from "./api/api";
 import { useParams } from "react-router-dom";
 import BookDetailHooks from "./hooks/BookDetailHooks";
 import UpdateBookResponse from "./components/UpdateBookResponse";
+import "./css/Form.css"
+import { Typography } from "@mui/material";
 
 function UpdateBook() {
     const { id } = useParams()
@@ -32,37 +34,42 @@ function UpdateBook() {
     }
 
     return (
-        <div>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                {({ errors, touched, resetForm }) => (
-                    <Form>
-                        <div className="form-group">
-                            <label htmlFor="title">Título</label>
-                            <Field name="title" id="title" type="text" className={'form-control' + (errors.title && touched.title ? 'is-invalid' : '')} />
-                            <ErrorMessage name="title" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="description">Descrição</label>
-                            <Field name="description" id="description" type="text" className={'form-control' + (errors.description && touched.description ? 'is-invalid' : '')} />
-                            <ErrorMessage name="description" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="excerpt">Excerto</label>
-                            <Field name="excerpt" id="excerpt" type="text" className={'form-control' + (errors.excerpt && touched.excerpt ? 'is-invalid' : '')} />
-                            <ErrorMessage name="excerpt" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="pageCount">Número de Páginas</label>
-                            <Field name="pageCount" id="pageCount" type="number" className={'form-control' + (errors.pageCount && touched.pageCount ? 'is-invalid' : '')} />
-                            <ErrorMessage name="pageCount" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit">Enviar</button>
-                            <button type="button" onClick={resetForm}>Limpar</button>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
+        <div className="form-area">
+            <div className="form">
+                <Typography variant="h4">
+                    Formulário de Atualização de Livro
+                </Typography>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                    {({ errors, touched, resetForm }) => (
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="title">Título</label>
+                                <Field name="title" id="title" type="text" className={'form-control' + (errors.title && touched.title ? 'is-invalid' : '')} />
+                                <ErrorMessage name="title" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="description">Descrição</label>
+                                <Field name="description" id="description" type="text" className={'form-control' + (errors.description && touched.description ? 'is-invalid' : '')} />
+                                <ErrorMessage name="description" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="excerpt">Excerto</label>
+                                <Field name="excerpt" id="excerpt" type="text" className={'form-control' + (errors.excerpt && touched.excerpt ? 'is-invalid' : '')} />
+                                <ErrorMessage name="excerpt" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="pageCount">Número de Páginas</label>
+                                <Field name="pageCount" id="pageCount" type="number" className={'form-control' + (errors.pageCount && touched.pageCount ? 'is-invalid' : '')} />
+                                <ErrorMessage name="pageCount" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group" id="btns">
+                                <button type="submit">Enviar</button>
+                                <button type="button" onClick={resetForm}>Limpar</button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </div>
     );
 }
